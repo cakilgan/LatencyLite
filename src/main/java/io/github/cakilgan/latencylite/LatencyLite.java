49f71482d17a83ad06a3e1156170a11b4ddc01dc
Çakılgan <eneskobal18@gmail.com>
2025-04-16 11:52:22 +0300

    version

--- src/main/java/io/github/cakilgan/latencylite/LatencyLite.java
+++ src/main/java/io/github/cakilgan/latencylite/LatencyLite.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 public class LatencyLite {
     public static final String MODID = "latencylite";
     public static final String MOD_NAME= "LatencyLite";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public LatencyLite(FMLJavaModLoadingContext context){
